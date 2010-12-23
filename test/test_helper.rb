304684c843b1d669cc0302eab09c82afa5b496e3
Greg Hansen <grhansen@gmail.com>
2010-12-22 22:52:07 -0600

    add riak/ripple integration files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,12 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# Setup in-memory test server for Riak
+require File.expand_path('../ripple_test_helper.rb', __FILE__)
+
 class ActiveSupport::TestCase
+  teardown { Ripple::TestServer.clear }
+
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
